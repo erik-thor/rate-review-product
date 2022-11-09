9470d99708551a6302d59988b3d7becf06ca79b1
Erik Thor <erik@van-ons.nl>
2022-11-09 16:24:39 +0100

    [Star-component] Added toggleable star rating

--- src/general/card.js
+++ src/general/card.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import thumbnail from './images/img.webp';
+import StarRating from './product-meta';
 
 class App extends Component {
     render() {
       return (
         <div className="product">
           <div className="product-header">
-            <div className="product-meta">
-            </div>
+            <StarRating />
             <img src={thumbnail} className="product-image" alt="product" />
           </div>
           <h2 className="product-title">Productname</h2>
