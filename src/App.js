f780377998b4aae3a489d00d2c2ca95ac4429fac
Erik Thor <erik@van-ons.nl>
2022-11-11 09:37:20 +0100

    [0.2] Comment form functionality and API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import Product from './general/card.js'
+import Product from './general/Card.js'
+import Comments from "./general/Comments";
 
 class App extends Component {
   render() {
     return (
-      <Product />
+      <div>
+        <Product />
+        <Comments
+          commentsUrl="http://localhost:3000/comments"
+          currentUserId="1" />
+      </div>
     );
   }
 }
--- src/general/card.js
+++ src/general/card.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
 import thumbnail from './images/img.webp';
-import StarRating from './product-meta';
 
-class App extends Component {
+class Card extends Component {
     render() {
       return (
         <div className="product">
           <div className="product-header">
-            <StarRating />
             <img src={thumbnail} className="product-image" alt="product" />
           </div>
           <h2 className="product-title">Productname</h2>
           <p className="product-description">
             Lorem ipsum dolor et.
           </p>
-      </div>
+        </div>
       );
     }
 }
 
-export default App;
+export default Card;
 
